8f40b38e60411b837dc36da9249ddd0bc9c7e9b8
VicenteLavagnino <vicente.lavagnino@uc.cl>
2024-03-31 14:45:15 -0300

    avance

--- Tarea 1/main.js
+++ Tarea 1/main.js
@@ -1,6 +1,6 @@
-const { Row } = require("react-bootstrap");
-
 const APPLE = "https://gist.githubusercontent.com/Hernan4444/d500438113d5eedc297f9c207fb03337/raw/474e0494400b27b96f381b04048deab5c8c586b9/Apple.csv"
+const SONY = "https://gist.githubusercontent.com/Hernan4444/d500438113d5eedc297f9c207fb03337/raw/474e0494400b27b96f381b04048deab5c8c586b9/Sony.csv"
+
 const data_path = APPLE;
 
 
@@ -8,11 +8,6 @@ const data_path = APPLE;
 
 const WIDTH = 900;
 const HEIGHT = 600;
-
-const svg = d3.select("#vis")
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT);
-
 
 // Función para parsear los datos, luego esto se lo pasamos a d3.csv
 const parseo = row => {
@@ -27,9 +22,11 @@ const parseo = row => {
 
 // Función para leer y preprocesar los datos
 function preprocessingBarchartDataset(data) {
+
     // console.log(data);
 
     d3.csv(data, parseo).then(function (data) {
+
         // Ver los datos parseados
         // console.log(data);
 
@@ -45,94 +42,63 @@ function preprocessingBarchartDataset(data) {
 function barplot(parsed_data) {
 
     // Ver los datos
-    console.log(parsed_data);
+    // console.log(parsed_data);
 
     // SETEAR LA VISUALIZACION
 
-    const margin = { top: 20, right: 30, bottom: 30, left: 50 },
-
+    const margin = { top: 20, right: 50, bottom: 30, left: 50 },
         width = WIDTH - margin.left - margin.right,
         height = HEIGHT - margin.top - margin.bottom;
 
 
-    // Máximo para los datos según el atributo "max"
-    let max_point = d3.max(parsed_data, d => d.max);
+    // ESCALAS
+    const escala_horizontal = d3.scaleTime().domain(d3.extent(parsed_data, d => d.date)).range([0, width]);
+    const escala_vertical = d3.scaleLinear().domain([0.9 * d3.min(parsed_data, d => Math.min(d.min, d.start, d.end)), 1.1 * d3.max(parsed_data, d => Math.max(d.max, d.start, d.end))]).range([height, 0]);
 
-    // definimos las escalas
-    let escala_horizontal = d3.scaleLinear().domain([0, max_point * 1.1]).range([0, WIDTH]);
-    let escala_vertical = d3.scaleBand().domain(parsed_data.map(d => d.date)).range([0, HEIGHT]);
+    // SVG
+    const svg = d3.select("#vis")
+        .attr("width", WIDTH)
+        .attr("height", HEIGHT)
+        .append("g")
+        .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    // Definimos los ejes en relación a las escalas
-    let ejeX = d3.axisBottom(escala_horizontal);
-    let ejeY = d3.axisLeft(escala_vertical);
+    // TITULO
 
-    // Creamos nuestro primer RECT que será el fondo
-    svg.append("rect")
-        .attr("x", 0) // Definimos atributo X
-        .attr("y", 0) // Definimos atributo Y
-        .attr("width", WIDTH) // Definimos atributo "ancho"
-        .attr("height", HEIGHT) // Definimos atributo "largo"
-        .attr("fill", " #D2EBF1") // Definimos el relleno
-        .attr("stroke", "black"); // Definimos el color del borde
+    svg.append("text")
+        .text("Acciones Apple")
 
-    // Agregamos el eje X. Para esto usamos call y d3.axisBottom(escala_horizontal)
+
+    // EJES X e Y EN RELACION A LAS ESCALAS
     svg.append("g")
-        .attr("id", "ejeX") // Le damos un ID
-        .attr("transform", `translate(0, ${HEIGHT})`) // Trasladamos el G
-        .call(ejeX); // Usamos call para crear el eje
+        .attr("transform", `translate(0,${height})`)
+        .call(d3.axisBottom(escala_horizontal));
 
-    //Agregamos el eje Y. Para esto usamos call y d3.axisLeft(escala_vertical)
     svg.append("g")
-        .attr("id", "ejeY") // Le damos un ID
-        .attr("transform", `translate(0, 0)`) // Trasladamos el G
-        .call(ejeY); // Usamos call para crear el eje
+        .call(d3.axisLeft(escala_vertical));
 
-    // Seleccionamos nuestro eje X y luego cada linea
-    svg.select("#ejeX")
-        .selectAll("line")
-        .attr("y2", -HEIGHT) // Definimos la longitud de las lineas
-        /* Notar que este valor es negativo porque tomamos el eje x como referencia, 
-        que ya tiene un valor en el eje y, y lo estamos definiendo respecto a eso. */
-        .attr("stroke", "black") // Definimos el color de la línea
-        .attr("stroke-width", 1.5) // Definimos en ancho de la línea
-        .attr("stroke-dasharray", "5,5") // Extra: definimos que será punteada
-        .attr("opacity", (_, i) => {
-            if (i === 0) {
-                return 0;
-            } else {
-                return 0.5;
-            }
-        });
-
-    /* Esto último hace un poco opaca la línea del eje, pero la opacidad es 0 para la primera línea, cosa que no se sobreponga con el eje Y */
-
-    // Extra
-    // Se pueden esconder los labels del eje y.
-    // d3.select("#ejeY").selectAll("text").attr('opacity', 0)
-
-    // Seleccionamos nuestro EjeY y luego cada texto (los labels)
-    d3.select("#ejeY")
-        .selectAll("text")
-        .attr("font-size", 15) // Definimos el tamaño de la fuente
-        .attr("font-weight", "bold") // Lo hacemos más negro
-        .attr("font-family", "monospace"); //Hacemos que sea mono espaciado, es decir, cada letra usa el mismo espacio
-
-    // Bonus
-    // Tambien se puede asignar una clase para aplicar el estilo en css
-    // d3.select("#ejeY").selectAll("text").attr('class', 'labelsY');
-
-    // GRAFICAR DATOS
+    // LINEA MAX y MIN
+    svg.selectAll("difference-line")
+        .data(parsed_data)
+        .enter()
+        .append("line")
+        .attr("x1", d => escala_horizontal(d.date))
+        .attr("y1", d => escala_vertical(d.max))
+        .attr("x2", d => escala_horizontal(d.date))
+        .attr("y2", d => escala_vertical(d.min))
+        .attr("stroke", "black")
+        .attr("stroke-width", 2);
 
     // BARRAS
-    // Creamos un G, le damos el id "barsG"
-    let barsG = SVG.append("g").attr("id", "barsG");
-
-    // Usamos la variable "barsG" que tiene nuestro G y aplicamos data-join
-    barsG
-        .selectAll("rect") // Buscamos cada rect
-        .data(parsed_data) // Aplicamos data-join entre los rect y los datos
-        .join("rect") // Creamos un rect para cada dato del conjunto "ENTER"
-
+    svg.selectAll(".bar")
+        .data(parsed_data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", d => escala_horizontal(d.date) - 2)
+        .attr("y", d => escala_vertical(Math.max(d.start, d.end)))
+        .attr("width", 5)
+        .attr("height", d => Math.abs(escala_vertical(d.start) - escala_vertical(d.end)))
+        .attr("fill", d => d.start > d.end ? "red" : "green")
+        .attr("stroke", "black");
 }
 
 /* Llamamos a nuestra función encargada de procesar los datos, que a su vez se encarga de llamar
